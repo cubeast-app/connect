0bc9ccacfa288485776643d3fba542ce4a8a250e
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2023-05-19 23:26:43 +0200

    Limit Websocket clients Origin to trusted domains Fixes #21

--- src-tauri/src/server/server.rs
+++ src-tauri/src/server/server.rs
@@ -2,9 +2,23 @@ use std::{env, sync::Arc};
 
 use crate::{controller::Controller, server::server_connection::ServerConnection};
 use futures_util::StreamExt;
+use http::{Response as HttpResponse, Uri};
 use log::info;
 use tokio::{net::TcpListener, sync::Mutex};
-use tokio_tungstenite;
+use tokio_tungstenite::{
+    self,
+    tungstenite::handshake::{
+        client::Request,
+        server::{ErrorResponse, Response},
+    },
+};
+
+pub const ALLOWED_HOSTS: [&str; 4] = [
+    "localhost",
+    "127.0.0.1",
+    "app.cubeast.com",
+    "app.staging.cubeast.com",
+];
 
 pub struct Server;
 
@@ -24,14 +38,35 @@ impl Server {
         );
 
         while let Ok((stream, _)) = listener.accept().await {
-            let ws_stream = tokio_tungstenite::accept_async(stream)
-                .await
-                .expect("Error during the websocket handshake occurred");
-            let (write, read) = ws_stream.split();
-            let write = Arc::new(Mutex::new(write));
-            let client_id = controller.add_client(write.clone()).await;
+            let ws_stream = tokio_tungstenite::accept_hdr_async(
+                stream,
+                |request: &Request, response: Response| -> Result<Response, ErrorResponse> {
+                    let origin = request.headers().get("origin");
 
-            ServerConnection::start(read, write, controller.clone(), client_id);
+                    if let Some(origin) = origin {
+                        let uri = origin.to_str().unwrap().parse::<Uri>().unwrap();
+                        let host = uri.host();
+
+                        if let Some(host) = host {
+                            if ALLOWED_HOSTS.contains(&host) {
+                                return Ok(response);
+                            }
+                        }
+                    }
+
+                    info!("Rejected connection from {:?}", origin);
+                    Err(HttpResponse::builder().status(403).body(None).unwrap())
+                },
+            )
+            .await;
+
+            if let Ok(ws_stream) = ws_stream {
+                let (write, read) = ws_stream.split();
+                let write = Arc::new(Mutex::new(write));
+                let client_id = controller.add_client(write.clone()).await;
+
+                ServerConnection::start(read, write, controller.clone(), client_id);
+            }
         }
     }
 }
