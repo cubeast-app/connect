e7701d34a4069917ab43cdfb463e7d225e3a6ffb
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2023-06-06 00:34:45 +0200

    Display app version (#33)
    Fixes #30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { invoke } from "@tauri-apps/api/tauri";
 import { BehaviorSubject, from, interval, mergeMap, Observable } from "rxjs";
 import { disable, enable, isEnabled } from "tauri-plugin-autostart-api";
+import { getVersion } from '@tauri-apps/api/app';
 
 const REFRESH_INTERVAL = 1000;
 
@@ -16,11 +17,13 @@ export class AppComponent implements OnInit, OnDestroy {
   @ViewChild(MatSlideToggle)
   private timerComponent!: MatSlideToggle;
   status!: Observable<number | string>;
+  appVersion!: string;
   faCheck = faCheck;
   faTimes = faTimes;
 
   ngOnInit(): void {
     this.status = interval(REFRESH_INTERVAL).pipe(mergeMap(() => this.refreshStatus()));
+    getVersion().then(version => this.appVersion = version);
     isEnabled().then(enabled => this.timerComponent.checked = enabled);
   }
   
